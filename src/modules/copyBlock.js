493bef3e3681893f7ad97f08733300eb0a151f3e
Manny Larios <manuelelarios@gmail.com>
2020-10-22 11:26:58 -0500

    Hello, Marty McFly!
    Testing syncing between computers.

--- src/modules/copyBlock.js
+++ src/modules/copyBlock.js
@@ -1,24 +1,29 @@
 // React
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 // Bootstrap Components
 import { Col, Container, Row } from "react-bootstrap";
 
 class CopyBlock extends Component {
-  render(){
-    return(
-        <Container>
-          <Row className="justify-content-center">
-            <Col xs={8}>
+  render() {
+    return (
+      <Container>
+        <Row className="justify-content-center">
+          <Col xs={8}>
             <div className="copyBlock">
-              <h1 className="h100">Hello, Marty!</h1>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident, distinctio. Iusto consectetur veniam magni ex! Cupiditate corporis et sunt eos culpa, laudantium ab, molestias, ipsam accusamus ut officiis impedit deserunt.</p>
+              <h1 className="h100">Hello, Marty McFly!</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Provident, distinctio. Iusto consectetur veniam magni ex!
+                Cupiditate corporis et sunt eos culpa, laudantium ab, molestias,
+                ipsam accusamus ut officiis impedit deserunt.
+              </p>
             </div>
-            </Col>
-          </Row>
-        </Container>
-    )
-}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default CopyBlock;
